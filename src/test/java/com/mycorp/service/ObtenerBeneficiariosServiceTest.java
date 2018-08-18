61dd1d9232970158019d45fbff96109110530bb8
Mayte <Mayte@Edu-Portatil.home>
2018-08-18 17:24:18 +0200

    add obtenerBeneficiarios test with principal use cases

---
+++ src/test/java/com/mycorp/service/ObtenerBeneficiariosServiceTest.java
@@ -0,0 +1,16 @@
+package com.mycorp.service;
+
+import org.junit.Test;
+
+public class ObtenerBeneficiariosServiceTest {
+
+	@Test
+	public void realizarIncluisionBeneficiariosTest() {
+		
+	}
+	
+	@Test
+	public void realizarAltaBeneficiarioTest() {
+		
+	}
+}
